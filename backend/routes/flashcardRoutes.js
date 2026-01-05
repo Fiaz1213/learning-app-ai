a5d86061325fe6a708b2d3e7bf60b3f889c69783
fiaz <fiaz89877@gmail.com>
2026-01-05 22:33:39 +0500

    setup flashcard routes and controller

---
+++ backend/routes/flashcardRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import {
+  getFlashcards,
+  getAllFlashcardSets,
+  reviewFlashcard,
+  toggleStarFlashcard,
+  deleteFlashcardSet,
+} from "../controllers/flashcardController.js";
+
+import protect from "../middleware/auth.js";
+
+const router = express.Router();
+
+// ALl routes are protected
+router.use(protect);
+
+router.get("/:documentId", getFlashcards);
+router.get("/", getAllFlashcardSets);
+
+router.post("/:cardId/review", reviewFlashcard);
+router.put("/:cardId/star", toggleStarFlashcard);
+
+router.delete("/:id", deleteFlashcardSet);
+
+export default router;
